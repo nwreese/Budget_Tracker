73a4d67556c02e9086fb516f6fa6522653bde52f
Nick Reese <nwreese@kent.edu>
2021-04-06 13:17:10 -0400

    updating readMe and indexedDb getall

--- public/indexedDb.js
+++ public/indexedDb.js
@@ -13,7 +13,10 @@ function checkForIndexedDb() {
     }
     return true;
   }
-  
+  function uploadTracker(){
+    useIndexedDb("budget", "budgetStore", "upload")
+  }
+
   function useIndexedDb(databaseName, storeName, method, object) {
     return new Promise((resolve, reject) => {
       const request = window.indexedDB.open(databaseName, 1);
@@ -43,7 +46,36 @@ function checkForIndexedDb() {
         }
         else if (method === "post") {
             store.add(object);
-        } else if (method === "get") {
+        } else if (method === "update"){
+    const getAll = store.getAll();
+     getAll.onsuccess = function() {
+     if (getAll.result.length > 0) {
+       fetch('/api/transaction', {
+         method: 'POST',
+         body: JSON.stringify(getAll.result),
+         headers: {
+           Accept: 'application/json, text/plain, */*',
+           'Content-Type': 'application/json'
+         }
+       })
+         .then(response => response.json())
+         .then(serverResponse => {
+           if (serverResponse.message) {
+             throw new Error(serverResponse);
+           }
+           const transaction = db.transaction(['new_budget'], 'readwrite');
+           const store = transaction.objectStore('new_budget');
+  
+          store.clear();
+           alert('All saved transactions has been submitted!');
+         })
+         .catch(err => {
+           console.log(err);
+         });
+     }
+   };
+        }
+         else if (method === "get") {
           const all = store.getAll();
           all.onsuccess = function() {
             resolve(all.result);
@@ -57,3 +89,4 @@ function checkForIndexedDb() {
       };
     });
   }
+window.addEventListener('online', uploadTracker);
